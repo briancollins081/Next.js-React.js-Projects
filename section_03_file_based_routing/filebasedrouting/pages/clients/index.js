51eb04a75ceb0e2a5f33a1ce4c220f29c286ae06
Brian Andere <briancollins081@gmail.com>
2021-03-11 23:25:02 +0300

    Using links to navigate daynamically

--- section_03_file_based_routing/filebasedrouting/pages/clients/index.js
+++ section_03_file_based_routing/filebasedrouting/pages/clients/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
+const clients = [
+  { id: "andere", name: "Andere Brian" },
+  { id: "collins", name: "Collins Brian" },
+  { id: "javis", name: "Javis Kim" },
+  { id: "jane", name: "Doe Jane" },
+];
 const Clients = () => {
   return (
     <div>
       <h1>Clients page</h1>
+      <ul>
+        {clients.map((cl) => (
+          <li key={cl.id}>
+            {/* <Link href={`/clients/${cl.id}`}>{cl.name}</Link> */}
+            <Link href={{ pathname: "/clients/[id]", query: {id: cl.id} }}>{cl.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
