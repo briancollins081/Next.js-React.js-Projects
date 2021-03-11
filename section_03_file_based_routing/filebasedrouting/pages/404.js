cf3956ceb7276bcb5d571d8195f012ff1e51bdb8
Brian Andere <briancollins081@gmail.com>
2021-03-11 23:37:26 +0300

    Add 404 page

---
+++ section_03_file_based_routing/filebasedrouting/pages/404.js
@@ -0,0 +1,7 @@
+const NotFoundPage = () => {
+    return <div>
+        <h1>404 Manenos here</h1>
+    </div>
+};
+
+export default NotFoundPage;
--- section_03_file_based_routing/filebasedrouting/pages/clients/[id]/index.js
+++ section_03_file_based_routing/filebasedrouting/pages/clients/[id]/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
 const ClientsProjects = () => {
+  const router = useRouter();
+  console.log(router.query);
+  const handleLoadProject = () => {
+    // router.replace("/clients/andere/project-a");
+    router.replace({
+      pathname: '/clients/[id]/[clientProjectId]',
+      query: {id:'andere', clientProjectId: 'project-a'}
+    });
+  };
   return (
     <div>
       <h1>All Selected Client's Projects</h1>
+      <button onClick={handleLoadProject}>
+        Load a Single Project(Project A)
+      </button>
     </div>
   );
 };
