2a552aaf32034b032a4d6aa796bc9f37d124820f
Brian Andere <briancollins081@gmail.com>
2021-03-13 17:41:26 +0300

    Using useSWR hook for client side requests

--- section_05_data_fetching/page_prerendering/pages/lastsales.js
+++ section_05_data_fetching/page_prerendering/pages/lastsales.js
@@ -1,35 +1,56 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const LastSales = () => {
-  const [sales, setSales] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [sales, setSales] = useState([]);
+  //   const [loading, setLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-4fce9-default-rtdb.firebaseio.com/sales.json"
+  );
 
   useEffect(() => {
-    setLoading(true);
-    fetch(
-      "https://nextjs-course-4fce9-default-rtdb.firebaseio.com/sales.json"
-    ).then((res) =>
-      res.json().then((data) => {
-        const temp = [];
-        for (const key in data) {
-          if (Object.hasOwnProperty.call(data, key)) {
-            temp.push({
-              id: key,
-              username: data[key].username,
-              volume: data[key].volume,
-            });
-          }
+    if (data) {
+      const temp = [];
+      for (const key in data) {
+        if (Object.hasOwnProperty.call(data, key)) {
+          temp.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
         }
-        setSales(temp);
-        setLoading(false);
-      })
-    );
-  }, []);
-  if (loading) {
-    return <p>Loading...</p>;
+      }
+      setSales(temp);
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     fetch(
+  //       "https://nextjs-course-4fce9-default-rtdb.firebaseio.com/sales.json"
+  //     ).then((res) =>
+  //       res.json().then((data) => {
+  //         const temp = [];
+  //         for (const key in data) {
+  //           if (Object.hasOwnProperty.call(data, key)) {
+  //             temp.push({
+  //               id: key,
+  //               username: data[key].username,
+  //               volume: data[key].volume,
+  //             });
+  //           }
+  //         }
+  //         setSales(temp);
+  //         setLoading(false);
+  //       })
+  //     );
+  //   }, []);
+  if (error) {
+    return <p>Failed to load!</p>;
   }
-  if(sales.length === 0){
-      return <p>No data yet</p>
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
   return (
     <ul>
