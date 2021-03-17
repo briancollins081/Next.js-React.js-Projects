918fe4c87bd12a510ef2fbea3878aeea0a6838a6
Brian Andere <briancollins081@gmail.com>
2021-03-17 23:24:29 +0300

    Using context api to manage app state - done

--- section_10_app_wide_state/events-app/components/input/comments.js
+++ section_10_app_wide_state/events-app/components/input/comments.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
+import NotificationContext from "../../store/NotificationContext";
+
 import classes from "./comments.module.css";
 
 function Comments(props) {
@@ -12,32 +14,71 @@ function Comments(props) {
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [lodingComment, setLoadingComments] = useState(false);
+
+  const notificationCtx = useContext(NotificationContext);
 
   const toggleCommentsHandler = () => {
     setShowComments((prevStatus) => !prevStatus);
   };
 
   const addCommentHandler = (commentData) => {
+    notificationCtx.showNotification({
+      title: "Comenting on the Event...",
+      message: "Creating new comment on the current event",
+      status: "pending",
+    });
     // send data to API
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
       headers: { "Content-Type": "application/json" },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            throw new Error(
+              data.message ||
+                "An error occurred during creation of your comment"
+            );
+          });
+        }
+      })
       .then((data) => {
-        console.log({ data });
+        notificationCtx.showNotification({
+          title: "Event comment success",
+          message: "Successfully created event comment!",
+          status: "success",
+        });
         displayComments();
+      })
+      .catch((err) => {
+        notificationCtx.showNotification({
+          title: "Event Comment Error",
+          message: "Failed creating event due to an error: " + err.message,
+          status: "error",
+        });
       });
+    const timer = setTimeout(() => {
+      notificationCtx.hideNotification();
+      clearTimeout(timer);
+    }, 3000);
   };
 
   const displayComments = () => {
+    setLoadingComments(true);
     fetch(`/api/comments/${eventId}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log({ data });
+        // console.log({ data });
         // setShowComments(true);
         setComments(data.comments);
+        const timer = setTimeout(() => {
+          setLoadingComments(false);
+          clearTimeout(timer);
+        }, 8000);
       });
   };
 
@@ -46,8 +87,14 @@ function Comments(props) {
       <button onClick={toggleCommentsHandler}>
         {showComments ? "Hide" : "Show"} Comments
       </button>
-      {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList list={comments} />}
+      {lodingComment && showComments ? (
+        <p>Fetching comments...</p>
+      ) : (
+        <>
+          {showComments && <NewComment onAddComment={addCommentHandler} />}
+          {showComments && <CommentList list={comments} />}
+        </>
+      )}
     </section>
   );
 }
--- section_10_app_wide_state/events-app/store/NotificationContext.js
+++ section_10_app_wide_state/events-app/store/NotificationContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NotificationContext = createContext({
   notification: null, //{title, message, status}
@@ -11,9 +11,21 @@ export default NotificationContext;
 export const NotificationContextProvider = ({ children }) => {
   const [activeNotification, setActiveNotification] = useState();
 
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => handleHideNotification(), 3000);
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   const handleShowNotification = (notificationData) => {
     setActiveNotification(notificationData);
   };
+  
   const handleHideNotification = () => {
     setActiveNotification(null);
   };
