b2dc2c744fd895978ea5b5608092ed23419cf07f
Brian Andere <briancollins081@gmail.com>
2021-03-20 02:04:14 +0300

    Finished contact form + notification bar

--- section_11_blog_project/pages/_app.js
+++ section_11_blog_project/pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <div id="notifications"></div>
     </Layout>
   );
 }
--- section_11_blog_project/pages/api/contact.js
+++ section_11_blog_project/pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -18,8 +20,32 @@ const handler = (req, res) => {
       email,
       message,
     };
-    console.log(newMessage);
-    res.status(201).json({ message: "Successfully send message!" });
+    let client;
+    try {
+      client = await new MongoClient("mongodb://localhost:27017", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }).connect();
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "There was an error connecting to database!" });
+      return;
+    }
+    if (client) {
+      const db = client.db("nextjs-course-blog");
+      try {
+        const result = await db.collection("contactus").insertOne(newMessage);
+        newMessage.id = result.insertedId;
+      } catch (error) {
+        res.status(500).json({ message: "Saving message failed!" });
+        client.close();
+        return;
+      }
+    }
+    res
+      .status(201)
+      .json({ message: "Successfully send message!", data: newMessage });
   }
 };
 export default handler;
