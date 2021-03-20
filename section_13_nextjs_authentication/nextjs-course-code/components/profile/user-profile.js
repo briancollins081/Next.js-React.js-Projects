cf81b7f56a4ded5b81f14e1771a7e83bbd41c17f
Brian Andere <briancollins081@gmail.com>
2021-03-20 14:25:04 +0300

    Using getSession for route protection

--- section_13_nextjs_authentication/nextjs-course-code/components/profile/user-profile.js
+++ section_13_nextjs_authentication/nextjs-course-code/components/profile/user-profile.js
@@ -1,9 +1,22 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession, getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(async () => {
+    const session = await getSession();
+    if (!session) {
+      window.location.href = "/auth";
+    }else{
+      setIsLoading(false);
+    }
+  }, []);
 
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
